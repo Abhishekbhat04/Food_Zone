eb230fd292c85244639497eb2614eab7d76205d4
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2023-12-29 17:21:49 +0530

    first commit

---
+++ src/pages/Success.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { PropagateLoader } from "react-spinners";
+
+const Success = () => {
+   const [loading, setLoading] = useState(true);
+   useState(() => {
+      setTimeout(() => {
+         setLoading(false);
+      }, 3000);
+   }, []);
+   return (
+      <div className="flex items-center justify-center h-screen">
+         {loading ? (
+            <PropagateLoader color="#ffb703" />
+         ) : (
+            <div className="text-center ">
+               <h1 className="mb-2 text-xl font-semibold text-yellow">
+                  Order Successful!
+               </h1>
+               <p className="text-sm lg:text-lg">
+                  Your Order has been Successfully placed
+               </p>
+            </div>
+         )}
+      </div>
+   );
+};
+
+export default Success;
