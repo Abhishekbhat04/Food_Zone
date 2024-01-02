a8f7dbbc547f611024b50f2567f24c3c07021bd5
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2024-01-02 19:04:37 +0530

    add social media links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -49,7 +49,7 @@ const Footer = () => {
                <div className="w-auto p-8">
                   <div className="-m-1.5 flex flex-wrap">
                      <div className="w-auto p-1.5">
-                        <a href="#">
+                        <a href="https://www.facebook.com/mohitmaithanii">
                            <div className="flex items-center justify-center w-8 h-8 border border-gray-300 rounded-full hover:border-gray-400">
                               <svg
                                  width="8"
@@ -65,7 +65,7 @@ const Footer = () => {
                         </a>
                      </div>
                      <div className="w-auto p-1.5">
-                        <a href="#">
+                        <a href="https://twitter.com/mohitmaithaniii">
                            <div className="flex items-center justify-center w-8 h-8 border border-gray-300 rounded-full hover:border-gray-400">
                               <svg
                                  width="14"
@@ -81,7 +81,7 @@ const Footer = () => {
                         </a>
                      </div>
                      <div className="w-auto p-1.5">
-                        <a href="#">
+                        <a href="https://www.instagram.com/mohitmaithanii/">
                            <div className="flex items-center justify-center w-8 h-8 border border-gray-300 rounded-full hover:border-gray-400">
                               <svg
                                  width="16"
